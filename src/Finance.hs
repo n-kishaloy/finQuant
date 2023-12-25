aae0e83fada43f3860042f2fe76e2608d914be47
Kishaloy Neogi <nkishaloy@yahoo.com>
2023-12-25 15:15:48 +0200

    Version 0.0.1

--- src/Finance.hs
+++ src/Finance.hs
@@ -1,18 +1,51 @@
 module Finance
   ( yearFrac,
-    DayCountConvention,
+    DayCountConvention (..),
     dayToFloat,
     dayFromFloat,
     Period,
     yrFrac,
+    Currency (..),
+    disFact,
+    xdisFact,
+    forwardRate,
+    pv,
+    fv,
+    invPeriod,
+    xpv,
+    newtRaph,
+    xfv,
+    pmt,
+    pvAnnuity,
+    npvT,
+    npv,
+    xnpv,
+    irr,
+    xirr,
+    yearFracVec,
   )
 where
 
 import Data.Time (Day, diffDays, toGregorian)
 import Data.Time.Calendar.OrdinalDate (Year, fromOrdinalDate, isLeapYear, toOrdinalDate)
+import Data.Vector qualified as V
+import Data.Vector.Unboxed qualified as U
 import GHC.Generics (Generic)
 
 type Period = (Day, Day)
+
+invPeriod :: Period -> Period
+invPeriod (x0, x1) = (x1, x0)
+
+newtRaph :: (Double -> Double) -> Double -> Double -> Maybe Double
+newtRaph f x xtol = nRx x (100 :: Int)
+  where
+    dx = xtol / 10.0
+    nRx _ 0 = Nothing
+    nRx y n = if abs dy < xtol then Just (y - dy) else nRx (y - dy) (n - 1)
+      where
+        z = f y
+        dy = dx * z / (f (y + dx) - z)
 
 daysInYear :: Year -> Double
 daysInYear y = if isLeapYear y then 366.0 else 365.0
@@ -27,14 +60,13 @@ dayFromFloat yr =
   let y = floor (yr - 0.00274)
    in fromOrdinalDate y (round ((yr - fromIntegral y) * daysInYear y))
 
-{-Enum defining different Days convention
-
-- US30360 => US 30/360 or NASD 30/360
-- EU30360 => EURO 30/360
-- ACTACT => (Actual days in Leap year) / 366 + (Actual days in Normal year) / 365
-- ACT360 => Actual nos of days / 360
-- ACT365 => Actual nos of days / 365
--}
+-- | Enum defining different Days convention
+--
+-- - US30360 => US 30/360 or NASD 30/360
+-- - EU30360 => EURO 30/360
+-- - ACTACT => Actual days in Leap year / 366 + Actual days in Normal year / 365
+-- - ACT360 => Actual nos of days / 360
+-- - ACT365 => Actual nos of days / 365
 data DayCountConvention
   = US30360
   | EU30360
@@ -43,23 +75,22 @@ data DayCountConvention
   | ACT365
   deriving (Eq, Show, Ord, Generic, Enum, Bounded)
 
-{- @yearFrac dayCon d0 d1 = Day difference (d1 - d0) in fraction of a year@
-
-- dayConv = Days Convention
-- d0 = reference date
-- d1 = target date
-
-Following methods are supported
-- US30360 => US 30/360 or NASD 30/360
-- EU30360 => EURO 30/360
-- ACTACT => (Days in Leap year) / 366 + (Days in Normal year) / 365
-- ACT360 => Actual nos of days / 360
-- ACT365 => Actual nos of days / 365
-
-Note that the ACTACT formula is different from MS Excel and follows the Actual/Actual ISDA rule. For more details refer <https://en.wikipedia.org/wiki/Day_count_convention>.
-
-The yearfrac function is also signed with the result coming as negative in case dt0 > dt1. This is different from MS Excel, where the yearfrac number return absolute difference between the dates.
--}
+-- | @yearFrac dayCon d0 d1 = Day difference (d1 - d0) in fraction of a year@
+--
+-- - dayConv = Days Convention
+-- - d0 = reference date
+-- - d1 = target date
+--
+-- Following methods are supported
+-- - US30360 => US 30/360 or NASD 30/360
+-- - EU30360 => EURO 30/360
+-- - ACTACT => Days in Leap year / 366 + Days in Normal year / 365
+-- - ACT360 => Actual nos of days / 360
+-- - ACT365 => Actual nos of days / 365
+--
+-- Note that the ACTACT formula is different from MS Excel and follows the Actual/Actual ISDA rule. For more details refer <https://en.wikipedia.org/wiki/Day_count_convention>.
+--
+-- The yearfrac function is also signed with the result coming as negative in case dt0 > dt1. This is different from MS Excel, where the yearfrac number return absolute difference between the dates.
 yearFrac :: DayCountConvention -> Day -> Day -> Double
 yearFrac ACT360 d0 d1 = fromIntegral (d1 `diffDays` d0) / 360.0
 yearFrac ACT365 d0 d1 = fromIntegral (d1 `diffDays` d0) / 365.0
@@ -70,7 +101,7 @@ yearFrac dayConv d0 d1 =
       (y1, m1, dt1) = toGregorian d1
    in case dayConv of
         EU30360 ->
-          let lastDay d = if d == 0 then 30 else d
+          let lastDay d = if d == 31 then 30 else d
            in dayCountFactor y0 m0 (lastDay dt0) y1 m1 (lastDay dt1)
         US30360 ->
           let lsFeb y m d = m == 2 && if isLeapYear y then d == 29 else d == 28
@@ -82,5 +113,120 @@ yearFrac dayConv d0 d1 =
               dy0 = if dx0 == 31 then 30 else dx0
            in dayCountFactor y0 m0 dy0 y1 m1 dy1
 
+yearFracVec :: DayCountConvention -> V.Vector Day -> Day -> U.Vector Double
+yearFracVec dayConv di d0 = U.convert $ V.map (yearFrac dayConv d0) di
+
 yrFrac :: Period -> Double
 yrFrac (d0, d1) = yearFrac US30360 d0 d1
+
+data Currency
+  = INR
+  | USD
+  | NGN
+  | EUR
+  | GBP
+  | CNY
+  | MZN
+  | ZAR
+  deriving (Eq, Show, Ord, Generic, Enum, Bounded)
+
+-- | Discount factor = 1/(1+r)^n
+-- - r = rate per period
+-- - n = nos of periods
+disFact :: Double -> Double -> Double
+disFact r n = 1.0 / (1.0 + r) ** n
+
+-- | Discount Factor between period = 1 / (1+r) ** (yrFrac d0 d1)
+-- - r  = rate for 1 year during period (d0, d1)
+-- - (d0, d1) = (begin date, end date)
+xdisFact :: Double -> Period -> Double
+xdisFact r p = disFact r (yrFrac p)
+
+-- | Forward rate between t0 and t1 given as Double
+-- - (r0, t0) = Tuple of Rate and Time in (0, t0)
+-- - (r1, t1) = Tuple of Rate and Time in (0, t1)
+forwardRate :: (Double, Double) -> (Double, Double) -> Double
+forwardRate (r0, t0) (r1, t1) = disFact r1 t1 / disFact r0 t0
+
+-- | PV of a Future cash flow
+-- - r  = Effective rate of return
+-- - n  = number of periods
+-- - f  = Future cash flow
+pv :: Double -> Double -> Double -> Double
+pv r n f = f / (1.0 + r) ** n
+
+-- | FV of a Present cash flow
+-- - r  = Effective rate of return
+-- - n  = number of periods
+-- - p  = Present cash flow
+fv :: Double -> Double -> Double -> Double
+fv r n p = p * (1.0 + r) ** n
+
+-- | PV of a Future cash flow
+-- - r  = Effective rate of return
+-- - pr = Period of discounting
+-- - f = Future cash flow
+xpv :: Double -> Period -> Double -> Double
+xpv r pr = pv r (yrFrac pr)
+
+-- | FV of a Present cash flow
+-- - r  = Effective rate of return
+-- - pr = Period of discounting
+-- - p = Present cash flow
+xfv :: Double -> Period -> Double -> Double
+xfv r pr = fv r (yrFrac pr)
+
+-- | Payment to cover the PV of an Annuity
+-- - r  = rate of return per period
+-- - n  = number of periods (say, years)
+-- - p  = PV of Annuity
+-- - f  = FV of Residual value that may be received
+pmt :: Double -> Double -> Double -> Double -> Double
+pmt r n p f = -(p + f / rn) * r / (1.0 - 1.0 / rn)
+  where
+    rn = (1.0 + r) ** n
+
+-- | PV of an annuity
+-- - r   = rate of return per period
+-- - n   = number of periods (say, years)
+-- - pm  = payment made in each transaction
+-- - f  = FV of Residual value that may be received
+pvAnnuity :: Double -> Double -> Double -> Double -> Double
+pvAnnuity r n pm f = -pm / r * (1.0 - 1.0 / rn) - f / rn
+  where
+    rn = (1.0 + r) ** n
+
+-- | NPV of cash flows against time given in periods @ time = 0.0
+-- - r   = rate of return across the periods
+-- - ti  = vector of time of cash flows given as Double
+-- - cf  = vector of corresponding cash flows
+npvT :: Double -> U.Vector Double -> U.Vector Double -> Double
+npvT r ti cf = U.sum $ U.zipWith (\t c -> c / r1 ** t) ti cf where r1 = 1.0 + r
+
+-- | NPV of cash flows against time given in periods @ time = t0
+-- - r   = rate of return across the periods
+-- - ti  = vector of time of cash flows given as Double
+-- - t0  = time period at which the NPV is sought. Essentially, NPV(ti - t0)
+-- - cf  = vector of corresponding cash flows
+npv :: Double -> U.Vector Double -> Double -> U.Vector Double -> Double
+npv r ti t0 cf = npvT r ti cf * (1.0 + r) ** t0
+
+-- | NPV of cash flows against time given by Date
+-- - r   = rate of return across the years
+-- - di  = vector of time of cash flows given as Date
+-- - d0  = Date at which the NPV is sought.
+-- - cf  = vector of corresponding cash flows
+xnpv :: Double -> V.Vector Day -> Day -> U.Vector Double -> Double
+xnpv r di d0 = npvT r (yearFracVec US30360 di d0)
+
+-- | IRR of cash flow against time given in periods
+-- - ti  = vector of time of cash flows given as Double
+-- - cf  = vector of corresponding cash flows
+irr :: U.Vector Double -> U.Vector Double -> Maybe Double
+irr ti cf = newtRaph (\r -> npvT r ti cf) 0.1 1e-6
+
+-- | IRR of cash flow against time given as Date
+-- - di  = vector of time of cash flows given as Date
+-- - cf  = vector of corresponding cash flows
+xirr :: V.Vector Day -> U.Vector Double -> Maybe Double
+xirr di = irr (yearFracVec US30360 di (di V.! 0))
--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,11 @@
 module Main (main) where
 
+import Data.Approx
 import Data.List (foldl')
+import Data.Text (Text)
+import Data.Time (Day, fromGregorian)
+import Finance (DayCountConvention (..), yearFrac)
+import Finance qualified as F
 
 qTest :: (Show a) => a -> [Bool] -> IO ()
 qTest nam x = putStrLn $ lJus 30 '.' nam ++ " => Tests :" ++ rJus 3 ' ' (p + f) ++ fl f
@@ -18,4 +23,112 @@ qCheck nam False = putStrLn $ "*** Error *** : " ++ show nam ++ "\n"
 qCheck _ _ = putStr ""
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = do
+  qCheck ("Test" :: Text) True
+
+  qTest
+    ("YearFrac" :: Text)
+    [ ( ( \(dt0, dt1) ->
+            ( yearFrac US30360 dt0 dt1,
+              yearFrac ACTACT dt0 dt1,
+              yearFrac ACT360 dt0 dt1,
+              yearFrac ACT365 dt0 dt1,
+              yearFrac EU30360 dt0 dt1
+            )
+        )
+          <$> ( [ (fromGregorian 2018 2 5, fromGregorian 2023 5 14),
+                  (fromGregorian 2020 2 29, fromGregorian 2024 2 28),
+                  (fromGregorian 2015 8 30, fromGregorian 2010 3 31),
+                  (fromGregorian 2016 2 28, fromGregorian 2016 10 30),
+                  (fromGregorian 2014 1 31, fromGregorian 2014 8 31),
+                  (fromGregorian 2014 2 28, fromGregorian 2014 9 30),
+                  (fromGregorian 2016 2 29, fromGregorian 2016 6 15)
+                ] ::
+                  [(Day, Day)]
+              )
+      )
+        =~ [ ( 5.27500000000000,
+               5.26849315068489,
+               5.34444444444444444,
+               5.271232876712329,
+               5.27500000000000
+             ),
+             ( 3.9944444444444444444444,
+               3.9972677595626465,
+               4.0555555555555555555555,
+               4.00000000000000,
+               3.99722222222222222222222
+             ),
+             ( -5.4166666666666666666,
+               -5.4164383561642350000,
+               -5.4944444444444444444,
+               -5.4191780821917810000,
+               -5.4166666666666666666
+             ),
+             ( 0.6722222222222222222,
+               0.6693989071038686000,
+               0.6805555555555555555,
+               0.6712328767123288000,
+               0.6722222222222222222
+             ),
+             ( 0.5833333333333333333,
+               0.5808219178084073000,
+               0.5888888888888888888,
+               0.5808219178082191000,
+               0.5833333333333333333
+             ),
+             ( 0.5833333333333333333,
+               0.5863013698631221000,
+               0.5944444444444444444,
+               0.5863013698630137000,
+               0.5888888888888888888
+             ),
+             ( 0.2916666666666666666,
+               0.2923497267759103000,
+               0.2972222222222222222,
+               0.2931506849315068700,
+               0.2944444444444444444
+             )
+           ]
+    ]
+  qTest
+    ("Discount factor" :: Text)
+    [ F.disFact 0.09 3.0 =~ 0.7721834800610642,
+      F.xdisFact 0.09 (fromGregorian 2015 3 15, fromGregorian 2018 10 8) =~ 0.7355566392384189,
+      F.forwardRate (0.07, 1.0) (0.09, 3.0) =~ 0.8262363236653387
+    ]
+
+  qTest
+    ("Newton Raphson" :: Text)
+    [ F.newtRaph (\x -> (x - 3.0) * (x - 4.0)) 2.0 1e-6 =~ Just 3.0,
+      F.newtRaph (\x -> (x - 4.0) ** 2.0) 2.0 1e-6 =~ Just 3.9999990972409805,
+      F.newtRaph (\x -> (x - 4.0) ** 2.0 + 5.0) 2.0 1e-6 =~ Nothing
+    ]
+
+  qTest
+    ("NPV" :: Text)
+    [ F.pmt (0.08 / 12.0) (30.0 * 12.0) (-1000.0) 50.0 =~ 7.304096785187425,
+      F.pvAnnuity (0.08 / 12.0) (30.0 * 12.0) 7.304096785187425 50.0 =~ -1000.0,
+      F.xpv 0.08 (fromGregorian 2020 2 29, fromGregorian 2024 2 28) 5.638
+        =~ 4.1458705451340800,
+      F.xfv 0.08 (fromGregorian 2020 2 29, fromGregorian 2024 2 28) 5.638
+        =~ 7.6671578752761100,
+      F.npv 0.08 [0.25, 6.25, 3.5, 4.5, 1.25] (-0.45) [-6.25, 1.2, 1.25, 3.6, 2.5] =~ 0.36962283798505946,
+      F.xnpv
+        0.08
+        [fromGregorian 2012 2 25, fromGregorian 2012 6 28, fromGregorian 2013 2 15, fromGregorian 2014 9 18, fromGregorian 2015 2 20]
+        (fromGregorian 2012 1 10)
+        [-15.0, 5.0, 25.0, -10.0, 50.0]
+        =~ 44.165773653310936,
+      F.irr [0.125, 0.29760274, 0.49760274, 0.55239726, 0.812671233] [-10.25, -2.5, 3.5, 9.5, 1.25] =~ Just 0.31813386475187844,
+      F.irr [0.125, 0.29760274, 0.49760274, 0.55239726, 0.812671233] [10.25, 2.5, 3.5, 9.5, 1.25] =~ Nothing,
+      F.xirr
+        [ fromGregorian 2012 2 25,
+          fromGregorian 2012 6 28,
+          fromGregorian 2013 2 15,
+          fromGregorian 2014 9 18,
+          fromGregorian 2015 2 20
+        ]
+        [-115.0, 5.0, 25.0, -10.0, 200.0]
+        =~ Just 0.27845538159261773
+    ]
